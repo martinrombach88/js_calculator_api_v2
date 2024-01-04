f8cbdf433eea60bfeb97f7c12081aef0fd2f6fcf
Martin Rombach <martinrombach88@gmail.com>
2024-01-04 14:02:15 +0000

    testing progress

---
+++ testing/app.test.ts
@@ -0,0 +1,27 @@
+const request = require("supertest");
+const { app, server } = require("../index.ts");
+
+describe("Sample Test", () => {
+  it("should test that true === true", () => {
+    expect(true).toBe(true);
+  });
+});
+
+describe("Test Node Server, returns pong object", () => {
+  server;
+  // Test GET request to '/'
+  it("responds with status 200", async () => {
+    const response = await request(app).get("/");
+    expect(response.status).toBe(200);
+  });
+  it("responds with body {message: pong}", async () => {
+    const response = await request(app).get("/");
+    expect(response.body).toEqual({
+      message: "pong",
+    });
+  });
+
+  afterAll(() => {
+    server.close();
+  });
+});
