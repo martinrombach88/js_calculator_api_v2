99c8f4dd88d64bd36be0ab4e03bad6aaf75b1329
Martin Rombach <martinrombach88@gmail.com>
2024-01-08 21:24:25 +0000

    reverting types

--- index.ts
+++ index.ts
@@ -1,15 +1,15 @@
 import getDateString from "./src/api/helpers/getDateString";
 import getRouteString from "./src/api/helpers/getRouteString";
 import Calculator from "./src/api/calculator/calculator";
-import Express from "express";
 
+const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
-const app: Express.Application = Express();
 const port: number = 8080;
 const testCalc = new Calculator();
 
 app.use(bodyParser.json());
-app.use((req: Express.Request, res: Express.Response, next: any) => {
+app.use((req: any, res: any, next: any) => {
   //This cors rule allows anyone to use any route below
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET");
@@ -17,25 +17,23 @@ app.use((req: Express.Request, res: Express.Response, next: any) => {
   next();
 });
 
-app.get("/", (req: Express.Request, res: Express.Response) => {
+app.get("/", (req: any, res: any) => {
   res.json({ message: "pong" });
   // console.log(`${getRouteString("Home", port)} ${getDateString()}.`);
   //console.log(`Calculator says 1 + 1 =`, testCalc.calculate("1+1"));
 });
 
-app.post(
-  "/calculation-result/",
-  (req: Express.Response, res: Express.Response) => {
-    console.log(
-      `${getRouteString("Calculation", port)} Params: ${
-        req.body.expression
-      } has the result ${testCalc.calculate(
-        req.body.expression
-      )}. ${getDateString()}.`
-    );
-    res.json({ result: testCalc.calculate(req.body.expression) });
-  }
-);
+app.post("/calculation-result/", (req: any, res: any) => {
+  console.log(req.body.expression);
+  console.log(
+    `${getRouteString("Calculation", port)} Params: ${
+      req.body.expression
+    } has the result ${testCalc.calculate(
+      req.body.expression
+    )}. ${getDateString()}.`
+  );
+  res.json({ result: testCalc.calculate(req.body.expression) });
+});
 
 const server = app.listen(port);
 
