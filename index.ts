7829911c56bccd7f4672246e192086a1f5b8119f
Martin Rombach <martinrombach88@gmail.com>
2024-01-13 14:35:37 +0000

    config of ts, scrum3 plans

--- index.ts
+++ index.ts
@@ -3,30 +3,35 @@ import getRouteString from "./src/api/helpers/getRouteString";
 import Calculator from "./src/api/calculator/calculator";
 import express from "express";
 import bodyParser from "body-parser";
+import { Logger, ILogObj } from "tslog";
 
-//const express = require("express");
+// const log: Logger<ILogObj> = new Logger();
+// log.silly("I am a silly log.");
+
 const app = express();
-//const bodyParser = require("body-parser");
 const port: number = 8080;
 const testCalc = new Calculator();
+const logger: Logger<ILogObj> = new Logger();
 
 app.use(bodyParser.json());
 app.use((req: any, res: any, next: any) => {
   //This cors rule allows anyone to use any route below
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET");
+  res.setHeader("Access-Control-Allow-Methods", "POST");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 
 app.get("/", (req: any, res: any) => {
+  logger.log(1, `${getRouteString("Home", port)} ${getDateString()}.`);
   res.json({ message: "pong" });
+
   // console.log(`${getRouteString("Home", port)} ${getDateString()}.`);
   //console.log(`Calculator says 1 + 1 =`, testCalc.calculate("1+1"));
 });
 
 app.post("/calculation-result/", (req: any, res: any) => {
-  console.log(req.body.expression);
+  logger.silly("test");
   console.log(
     `${getRouteString("Calculation", port)} Params: ${
       req.body.expression
